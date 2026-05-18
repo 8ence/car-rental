e01e64c0bea911b7299c77fa4fe83663c44f13b8
Kővári Bence <kovaribence95@gmail.com>
2026-05-18 14:58:56 +0200

    Added initial seed data for rental company

--- main.py
+++ main.py
@@ -1,16 +1,14 @@
 from src.data import create_initial_rental_company
 from src.services.rental_service import RentalService
+from src.ui.console_ui import ConsoleUi
 
 
 def main():
     rental_company = create_initial_rental_company()
     rental_service = RentalService(rental_company)
 
-    print(rental_company)
-
-    print("\nInitial rentals:")
-    for rental in rental_service.list_rentals():
-        print(rental)
+    console_ui = ConsoleUi(rental_service, rental_company)
+    console_ui.start()
 
 
 if __name__ == "__main__":
---
+++ src/ui/console_ui.py
@@ -0,0 +1,81 @@
+class ConsoleUi:
+
+    def __init__(self, rental_service, rental_company):
+        self.__rental_service = rental_service
+        self.__rental_company = rental_company
+
+    def start(self):
+        while True:
+            print("\n=== Car Rental System ===")
+            print("1. List vehicles")
+            print("2. Rent vehicle")
+            print("3. Cancel rental")
+            print("4. List rentals")
+            print("0. Exit")
+
+            choice = input("Choose an option (Type the serial number): ")
+
+            if choice == "1":
+                self.__list_vehicles()
+            elif choice == "2":
+                self.__rent_vehicle()
+            elif choice == "3":
+                self.__cancel_rental()
+            elif choice == "4":
+                self.__list_rentals()
+            elif choice == "0":
+                print("Exiting... Goodbye!")
+            else:
+                print("Invalid option. Please choose one of the options above.")
+
+    def __list_vehicles(self):
+        print("\n=== List vehicles ===")
+        for vehicle in self.__rental_company.get_vehicles():
+            print(vehicle)
+
+    def __rent_vehicle(self):
+        try:
+            registration_number  = input("Enter registration number: ")
+            date = input("Enter date (YYYY-MM-DD): ")
+            renter = input("Enter name: ")
+
+            vehicle = self.__find_vehicle_by_registration_number(registration_number )
+
+            price = self.__rental_service.rent_vehicle(vehicle, date, renter)
+
+            print(f"Succesfull rental! Price: {price} Ft")
+
+        except Exception as ex:
+            print(ex)
+
+    def __cancel_rental(self):
+        try:
+            registration_number = input("Enter registration number: ")
+            date = input("Enter date (YYYY-MM-DD): ")
+
+            vehicle = self.__find_vehicle_by_registration_number(registration_number)
+
+            message = self.__rental_service.cancel_rental(vehicle, date)
+
+            print(message)
+
+        except Exception as ex:
+            print(ex)
+
+    def __list_rentals(self):
+        try:
+            print("\n=== List rentals ===")
+            for rental in self.__rental_service.list_rentals():
+                print(rental)
+
+        except Exception as ex:
+            print(ex)
+
+    def __find_vehicle_by_registration_number(self, registration_number):
+
+        for vehicle in self.__rental_company.get_vehicles():
+
+            if vehicle.get_registration_number() == registration_number:
+                return vehicle
+
+        raise Exception("Invalid registration number")
